50750f9068a2f3e04234078f882802ccbbd82e65
Pratheek Bedre <pratheekbedrejun20@gmail.com>
2025-06-16 23:26:38 +0530

    feat: add multiply function to calculator

--- app.py
+++ app.py
@@ -6,6 +6,11 @@ def subtract(a, b):
     return a - b
 
 
+def multiply(a, b):
+    return a * b
+
+
 if __name__ == "__main__":
     print(add(1, 2))
     print(subtract(1, 2))
+    print(multiply(1, 2))
