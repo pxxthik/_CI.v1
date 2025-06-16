a873c0f1a263e48ea9a9c6f155ac03b33f1c5f4e
Pratheek Bedre <pratheekbedrejun20@gmail.com>
2025-06-16 23:31:20 +0530

    feat: add divide function to calculator

--- app.py
+++ app.py
@@ -10,7 +10,12 @@ def multiply(a, b):
     return a * b
 
 
+def divide(a, b):
+    return a / b
+
+
 if __name__ == "__main__":
     print(add(1, 2))
     print(subtract(1, 2))
     print(multiply(1, 2))
+    print(divide(1, 2))
