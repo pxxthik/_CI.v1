9f550a077e3cf44c4adf13bc9b5dffc7c31d3518
Pratheek Bedre <pratheekbedrejun20@gmail.com>
2025-06-17 00:00:30 +0530

    reorganized

--- app.py
+++ app.py
@@ -15,7 +15,9 @@ def divide(a, b):
 
 
 if __name__ == "__main__":
-    print(add(1, 2))
-    print(subtract(1, 2))
-    print(multiply(1, 2))
-    print(divide(1, 2))
+    sum = add(1, 2)
+    diff = subtract(1, 2)
+    product = multiply(1, 2)
+    quotient = divide(1, 2)
+
+    print(sum, diff, product, quotient)
